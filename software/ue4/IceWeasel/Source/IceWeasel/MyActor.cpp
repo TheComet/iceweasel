65983c620186d30b04e1c6c782b258ae95e3df2c
newtechnology <abhijeetzala81@gmail.com>
2017-02-08 22:38:21 +0530

    removed custom gravity plugin

--- software/ue4/IceWeasel/Source/IceWeasel/MyActor.cpp
+++ software/ue4/IceWeasel/Source/IceWeasel/MyActor.cpp
@@ -1,11 +1,11 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "IceWeasel.h"
-#include "TestActor.h"
+#include "MyActor.h"
 
 
 // Sets default values
-ATestActor::ATestActor()
+AMyActor::AMyActor()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
@@ -13,20 +13,16 @@ ATestActor::ATestActor()
 }
 
 // Called when the game starts or when spawned
-void ATestActor::BeginPlay()
+void AMyActor::BeginPlay()
 {
 	Super::BeginPlay();
 	
 }
 
 // Called every frame
-void ATestActor::Tick( float DeltaTime )
+void AMyActor::Tick( float DeltaTime )
 {
 	Super::Tick( DeltaTime );
 
-	FVector NewLocation = FVector(0.0f, 0.0f, 0.0f);
-	NewLocation = GetActorLocation() * 1 * DeltaTime;
-
-	SetActorLocation(NewLocation);
 }
 
--- software/ue4/IceWeasel/Source/IceWeasel/MyActor.h
+++ software/ue4/IceWeasel/Source/IceWeasel/MyActor.h
@@ -3,16 +3,16 @@
 #pragma once
 
 #include "GameFramework/Actor.h"
-#include "TestActor.generated.h"
+#include "MyActor.generated.h"
 
 UCLASS()
-class ICEWEASEL_API ATestActor : public AActor
+class ICEWEASEL_API AMyActor : public AActor
 {
 	GENERATED_BODY()
 	
 public:	
 	// Sets default values for this actor's properties
-	ATestActor();
+	AMyActor();
 
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
