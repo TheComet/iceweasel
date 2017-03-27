0de05a6ab758fbf9fc5e9b683697d64831655822
newtechnology <abhijeetzala81@gmail.com>
2017-03-28 03:16:41 +0530

    more work on scoreboard

---
+++ software/ue4/IceWeasel/Source/IceWeasel/BasePlayerState.cpp
@@ -0,0 +1,10 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "IceWeasel.h"
+#include "BasePlayerState.h"
+
+
+ABasePlayerState::ABasePlayerState()
+{
+	
+}
